2b76c6f666bfd5e743a39b94a1713d2d16b91337
shreyas <spapinwar@gmail.com>
2021-06-17 23:11:00 +0530

    Ignore eslint for builds

--- web/next.config.js
+++ web/next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
-  useEslint: false
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
